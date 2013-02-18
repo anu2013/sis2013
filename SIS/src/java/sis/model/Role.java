ef15d8319124bb7affb7a2d6f57d631584aec733
Anupama Karumudi <anupamareddyk@gmail.com>
2013-02-18 11:38:19 -0500

    model/Role.java :: removed unnecessary relations.

--- SIS/src/java/sis/model/Role.java
+++ SIS/src/java/sis/model/Role.java
@@ -5,16 +5,13 @@
 package sis.model;
 
 import java.io.Serializable;
-import java.util.List;
+import javax.faces.bean.ManagedBean;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -22,12 +19,10 @@ import javax.validation.constraints.Size;
  *
  * @author Anupama Karumudi
  */
+@ManagedBean
 @Entity
 @Table(name = "ROLE")
-@NamedQueries({
-    @NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r")})
 public class Role implements Serializable {
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
@@ -36,8 +31,6 @@ public class Role implements Serializable {
     @Size(max = 100)
     @Column(name = "ROLENAME")
     private String rolename;
-    @OneToMany(mappedBy = "roleid")
-    private List<Users> usersList;
 
     public Role() {
     }
@@ -60,14 +53,6 @@ public class Role implements Serializable {
 
     public void setRolename(String rolename) {
         this.rolename = rolename;
-    }
-
-    public List<Users> getUsersList() {
-        return usersList;
-    }
-
-    public void setUsersList(List<Users> usersList) {
-        this.usersList = usersList;
     }
 
     @Override
