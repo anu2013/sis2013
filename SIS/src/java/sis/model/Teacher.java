139ed00dc87683841cf0bc2514483d95befba0f6
Anupama Karumudi <anupamareddyk@gmail.com>
2013-03-03 23:59:46 -0500

    model/Teacher.java :: updated the model to remove unnecessary getter, setters and added profile property.

--- SIS/src/java/sis/model/Teacher.java
+++ SIS/src/java/sis/model/Teacher.java
@@ -5,15 +5,12 @@
 package sis.model;
 
 import java.io.Serializable;
-import java.util.List;
+import javax.faces.bean.ManagedBean;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -23,10 +20,9 @@ import javax.validation.constraints.Size;
  *
  * @author Anupama Karumudi
  */
+@ManagedBean
 @Entity
 @Table(name = "TEACHER")
-@NamedQueries({
-    @NamedQuery(name = "Teacher.findAll", query = "SELECT t FROM Teacher t")})
 public class Teacher implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -34,24 +30,22 @@ public class Teacher implements Serializable {
     @NotNull
     @Column(name = "TEACHERID")
     private Integer teacherid;
+    
     @Size(max = 255)
     @Column(name = "EDUCATIONALQUALIFICATION")
     private String educationalqualification;
+    
     @Column(name = "YEARSOFEXPERIENCE")
     private Integer yearsofexperience;
+    
     @Size(max = 255)
     @Column(name = "SPECIALIZATION")
     private String specialization;
+
     @JoinColumn(name = "TEACHERID", referencedColumnName = "USERID", insertable = false, updatable = false)
     @OneToOne(optional = false)
-    private Users users;
-    @OneToMany(mappedBy = "teacherid")
-    private List<Previousworkhistory> previousworkhistoryList;
-    @OneToMany(mappedBy = "primaryteacherid")
-    private List<Subjectschedule> subjectscheduleList;
-    @OneToMany(mappedBy = "secondaryteacherid")
-    private List<Subjectschedule> subjectscheduleList1;
-
+    private Userprofile profile;
+    
     public Teacher() {
     }
 
@@ -91,61 +85,11 @@ public class Teacher implements Serializable {
         this.specialization = specialization;
     }
 
-    public Users getUsers() {
-        return users;
+    public Userprofile getProfile() {
+        return profile;
     }
 
-    public void setUsers(Users users) {
-        this.users = users;
-    }
-
-    public List<Previousworkhistory> getPreviousworkhistoryList() {
-        return previousworkhistoryList;
-    }
-
-    public void setPreviousworkhistoryList(List<Previousworkhistory> previousworkhistoryList) {
-        this.previousworkhistoryList = previousworkhistoryList;
-    }
-
-    public List<Subjectschedule> getSubjectscheduleList() {
-        return subjectscheduleList;
-    }
-
-    public void setSubjectscheduleList(List<Subjectschedule> subjectscheduleList) {
-        this.subjectscheduleList = subjectscheduleList;
-    }
-
-    public List<Subjectschedule> getSubjectscheduleList1() {
-        return subjectscheduleList1;
-    }
-
-    public void setSubjectscheduleList1(List<Subjectschedule> subjectscheduleList1) {
-        this.subjectscheduleList1 = subjectscheduleList1;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (teacherid != null ? teacherid.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Teacher)) {
-            return false;
-        }
-        Teacher other = (Teacher) object;
-        if ((this.teacherid == null && other.teacherid != null) || (this.teacherid != null && !this.teacherid.equals(other.teacherid))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "sis.model.Teacher[ teacherid=" + teacherid + " ]";
-    }
-    
+    public void setProfile(Userprofile profile) {
+        this.profile = profile;
+    }    
 }
