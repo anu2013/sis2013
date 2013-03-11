78809b03bf2ebf80a658e71194723a882ea7a9f0
Anupama Karumudi <anupamareddyk@gmail.com>
2013-03-11 04:33:31 -0400

    model/Studentscorecard.java :: removed unused properties.

--- SIS/src/java/sis/model/Studentscorecard.java
+++ SIS/src/java/sis/model/Studentscorecard.java
@@ -5,6 +5,7 @@
 package sis.model;
 
 import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -22,10 +23,9 @@ import javax.validation.constraints.Size;
  *
  * @author Anupama Karumudi
  */
+@ManagedBean
 @Entity
 @Table(name = "STUDENTSCORECARD")
-@NamedQueries({
-    @NamedQuery(name = "Studentscorecard.findAll", query = "SELECT s FROM Studentscorecard s")})
 public class Studentscorecard implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -33,27 +33,31 @@ public class Studentscorecard implements Serializable {
     @Basic(optional = false)
     @Column(name = "STUDENTSCORECARDID")
     private Integer studentscorecardid;
+    
     @Size(max = 100)
     @Column(name = "FINALSCORE")
     private String finalscore;
+    
     @Size(max = 255)
     @Column(name = "COMMENTS")
     private String comments;
+    
     @Size(max = 100)
     @Column(name = "STATUS")
     private String status;
+    
     @Size(max = 5)
     @Column(name = "GRADELETTER")
     private String gradeletter;
-    @JoinColumn(name = "SUBJECTID", referencedColumnName = "SUBJECTID")
-    @ManyToOne
-    private Subject subjectid;
-    @JoinColumn(name = "STUDENTID", referencedColumnName = "STUDENTID")
-    @ManyToOne
-    private Student studentid;
-    @JoinColumn(name = "SCHOOLYEAR", referencedColumnName = "SCHOOLYEAR")
-    @ManyToOne
-    private Schoolyearschedule schoolyear;
+    
+    @Column(name = "SUBJECTID")
+    private Integer subjectid;
+    
+    @Column(name = "STUDENTID")
+    private Integer studentId;
+    
+    @Column(name = "SCHOOLYEAR")
+    private Integer schoolyear;
 
     public Studentscorecard() {
     }
@@ -102,53 +106,27 @@ public class Studentscorecard implements Serializable {
         this.gradeletter = gradeletter;
     }
 
-    public Subject getSubjectid() {
+    public Integer getSubjectid() {
         return subjectid;
     }
 
-    public void setSubjectid(Subject subjectid) {
+    public void setSubjectid(Integer subjectid) {
         this.subjectid = subjectid;
     }
 
-    public Student getStudentid() {
-        return studentid;
+    public Integer getStudentId() {
+        return studentId;
     }
 
-    public void setStudentid(Student studentid) {
-        this.studentid = studentid;
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
     }
 
-    public Schoolyearschedule getSchoolyear() {
+    public Integer getSchoolyear() {
         return schoolyear;
     }
 
-    public void setSchoolyear(Schoolyearschedule schoolyear) {
+    public void setSchoolyear(Integer schoolyear) {
         this.schoolyear = schoolyear;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (studentscorecardid != null ? studentscorecardid.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Studentscorecard)) {
-            return false;
-        }
-        Studentscorecard other = (Studentscorecard) object;
-        if ((this.studentscorecardid == null && other.studentscorecardid != null) || (this.studentscorecardid != null && !this.studentscorecardid.equals(other.studentscorecardid))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "sis.model.Studentscorecard[ studentscorecardid=" + studentscorecardid + " ]";
-    }
-    
 }
