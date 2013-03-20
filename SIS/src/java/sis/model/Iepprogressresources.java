51d15651ae44137a50b63ddff4740a06eee1cb2c
Anupama Karumudi <anupamareddyk@gmail.com>
2013-03-20 03:39:06 -0400

    model/Iepprogressresources.java :: updated the model

--- SIS/src/java/sis/model/Iepprogressresources.java
+++ SIS/src/java/sis/model/Iepprogressresources.java
@@ -6,16 +6,13 @@ package sis.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.faces.bean.ManagedBean;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -25,10 +22,9 @@ import javax.validation.constraints.Size;
  *
  * @author Anupama Karumudi
  */
+@ManagedBean
 @Entity
 @Table(name = "IEPPROGRESSRESOURCES")
-@NamedQueries({
-    @NamedQuery(name = "Iepprogressresources.findAll", query = "SELECT i FROM Iepprogressresources i")})
 public class Iepprogressresources implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -36,18 +32,21 @@ public class Iepprogressresources implements Serializable {
     @Basic(optional = false)
     @Column(name = "IEPPROGRESSRESOURCEID")
     private Integer iepprogressresourceid;
+    
     @Size(max = 100)
     @Column(name = "RESOURCENAME")
     private String resourcename;
+    
     @Size(max = 255)
     @Column(name = "RESOURCEURL")
     private String resourceurl;
+    
     @Column(name = "UPLOADEDDATE")
     @Temporal(TemporalType.TIMESTAMP)
     private Date uploadeddate;
-    @JoinColumn(name = "IEPPROGRESSID", referencedColumnName = "IEPPROGRESSID")
-    @ManyToOne
-    private Iepprogress iepprogressid;
+    
+    @Column(name = "IEPPROGRESSID")
+    private Integer iepprogressid;
 
     public Iepprogressresources() {
     }
@@ -88,37 +87,11 @@ public class Iepprogressresources implements Serializable {
         this.uploadeddate = uploadeddate;
     }
 
-    public Iepprogress getIepprogressid() {
+    public Integer getIepprogressid() {
         return iepprogressid;
     }
 
-    public void setIepprogressid(Iepprogress iepprogressid) {
+    public void setIepprogressid(Integer iepprogressid) {
         this.iepprogressid = iepprogressid;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (iepprogressresourceid != null ? iepprogressresourceid.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Iepprogressresources)) {
-            return false;
-        }
-        Iepprogressresources other = (Iepprogressresources) object;
-        if ((this.iepprogressresourceid == null && other.iepprogressresourceid != null) || (this.iepprogressresourceid != null && !this.iepprogressresourceid.equals(other.iepprogressresourceid))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "sis.model.Iepprogressresources[ iepprogressresourceid=" + iepprogressresourceid + " ]";
-    }
-    
 }
