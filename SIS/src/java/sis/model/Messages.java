9f05565ce1da005a9f7ff3241a7acca0c6253f8c
Anupama Karumudi <anupamareddyk@gmail.com>
2013-04-02 12:00:54 -0400

    sis/model/Messages.java :: updated the model and removed unnecessary getters and setters.

--- SIS/src/java/sis/model/Messages.java
+++ SIS/src/java/sis/model/Messages.java
@@ -6,18 +6,13 @@ package sis.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
+import javax.faces.bean.ManagedBean;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -27,10 +22,9 @@ import javax.validation.constraints.Size;
  *
  * @author Anupama Karumudi
  */
+@ManagedBean
 @Entity
 @Table(name = "MESSAGES")
-@NamedQueries({
-    @NamedQuery(name = "Messages.findAll", query = "SELECT m FROM Messages m")})
 public class Messages implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -38,19 +32,17 @@ public class Messages implements Serializable {
     @Basic(optional = false)
     @Column(name = "MESSAGEID")
     private Integer messageid;
+    
     @Size(max = 255)
     @Column(name = "SUBJECT")
     private String subject;
+    
     @Column(name = "MESSAGECREATEDDATE")
     @Temporal(TemporalType.TIMESTAMP)
     private Date messagecreateddate;
-    @OneToMany(mappedBy = "messageid")
-    private List<Conversations> conversationsList;
-    @OneToMany(mappedBy = "messageid")
-    private List<Recipients> recipientsList;
-    @JoinColumn(name = "MESSAGECREATEDBY", referencedColumnName = "USERID")
-    @ManyToOne
-    private Users messagecreatedby;
+    
+    @Column(name = "MESSAGECREATEDBY")
+    private Integer messagecreatedby;
 
     public Messages() {
     }
@@ -83,53 +75,11 @@ public class Messages implements Serializable {
         this.messagecreateddate = messagecreateddate;
     }
 
-    public List<Conversations> getConversationsList() {
-        return conversationsList;
-    }
-
-    public void setConversationsList(List<Conversations> conversationsList) {
-        this.conversationsList = conversationsList;
-    }
-
-    public List<Recipients> getRecipientsList() {
-        return recipientsList;
-    }
-
-    public void setRecipientsList(List<Recipients> recipientsList) {
-        this.recipientsList = recipientsList;
-    }
-
-    public Users getMessagecreatedby() {
+    public Integer getMessagecreatedby() {
         return messagecreatedby;
     }
 
-    public void setMessagecreatedby(Users messagecreatedby) {
+    public void setMessagecreatedby(Integer messagecreatedby) {
         this.messagecreatedby = messagecreatedby;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (messageid != null ? messageid.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Messages)) {
-            return false;
-        }
-        Messages other = (Messages) object;
-        if ((this.messageid == null && other.messageid != null) || (this.messageid != null && !this.messageid.equals(other.messageid))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "sis.model.Messages[ messageid=" + messageid + " ]";
-    }
-    
 }
