0274d18a1256cca2edc1a04c6625f1c174e879d1
Veekija Thirumalai <veekija@gmail.com>
2013-04-10 07:10:22 -0400

    State report

--- SIS/src/java/sis/controller/StateReportController.java
+++ SIS/src/java/sis/controller/StateReportController.java
@@ -4,6 +4,7 @@
  */
 package sis.controller;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -30,6 +31,8 @@ public class StateReportController {
     private List<Schoolyearschedule> schoolyearschedules;
     private Integer selectedSchoolYear;
     private List<StateReportVO> stateReportVOs = null;
+    private Integer mathsSubjectId = 7;
+    private Integer readingSubjectId = 7;
 
     @PostConstruct
     public void init() {
@@ -47,10 +50,37 @@ public class StateReportController {
         }
     }
 
-   public String generateReport() {
+    public String generateReport() {
+
+        List<StateReportVO> stVOs = new ArrayList<StateReportVO>();
+        List<StateReportVO> raceDeatils = retrieveRaceDetails();
+        stVOs.addAll(raceDeatils);
+        this.setStateReportVOs(stVOs);
+        return null;
+    }
+
+    private List retrieveRaceDetails() {
 
         List<StateReportVO> stVOs = new ArrayList<StateReportVO>();
         StateReportVO stateReportVO = null;
+        int totalNumberOfStudents = 0;
+        int numberOfStudentsTested_Maths = 0;
+        double percentageOfStudentsTested_Maths = 0.00;
+        int numberOfStudentsScoredAtBasicLevel_Maths = 0;
+        double percentageOfStudentsScoredAtBasicLevel_Maths = 0.00;
+        int numberOfStudentsScoredAtProficentLevel_Maths = 0;
+        double percentageOfStudentsScoredAtProficentLevel_Maths = 0.00;
+        int numberOfStudentsScoredAtAdvanceLevel_Maths = 0;
+        double percentageOfStudentsScoredAtAdvanceLevel_Maths = 0.00;
+        int numberOfStudentsTested_Reading = 0;
+        double percentageOfStudentsTested_Reading = 0.00;
+        int numberOfStudentsScoredAtBasicLevel_Reading = 0;
+        double percentageOfStudentsScoredAtBasicLevel_Reading = 0.00;
+        int numberOfStudentsScoredAtProficentLevel_Reading = 0;
+        double percentageOfStudentsScoredAtProficentLevel_Reading = 0.00;
+        int numberOfStudentsScoredAtAdvanceLevel_Reading = 0;
+        double percentageOfStudentsScoredAtAdvanceLevel_Reading = 0.00;
+
         List<String> raceList = new ArrayList<String>();
         raceList.add("American Indian or Alaska Native");
         raceList.add("Asian");
@@ -59,25 +89,84 @@ public class StateReportController {
         raceList.add("White");
 
         for (String race : raceList) {
+            totalNumberOfStudents = retrieveTotalStudentsCount(race, "RACE");
+
+            numberOfStudentsTested_Maths = retrieveTotalStudentsTestedCount(race, "RACE", mathsSubjectId);
+            if (numberOfStudentsTested_Maths != 0) {
+                percentageOfStudentsTested_Maths = ((double) numberOfStudentsTested_Maths / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsTested_Maths = 0;
+            }
+
+            numberOfStudentsScoredAtBasicLevel_Maths = retrieveTotalStudentsScoredAtBasicLevel(race, "RACE", mathsSubjectId, "70", "79");
+            if (numberOfStudentsScoredAtBasicLevel_Maths != 0) {
+                percentageOfStudentsScoredAtBasicLevel_Maths = ((double) numberOfStudentsScoredAtBasicLevel_Maths / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsScoredAtBasicLevel_Maths = 0;
+            }
+
+            numberOfStudentsScoredAtProficentLevel_Maths = retrieveTotalStudentsScoredAtBasicLevel(race, "RACE", mathsSubjectId, "80", "89");
+            if (numberOfStudentsScoredAtProficentLevel_Maths != 0) {
+                percentageOfStudentsScoredAtProficentLevel_Maths = ((double) numberOfStudentsScoredAtProficentLevel_Maths / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsScoredAtProficentLevel_Maths = 0;
+            }
+
+            numberOfStudentsScoredAtAdvanceLevel_Maths = retrieveTotalStudentsScoredAtBasicLevel(race, "RACE", mathsSubjectId, "90", "99");
+            if (numberOfStudentsScoredAtAdvanceLevel_Maths != 0) {
+                percentageOfStudentsScoredAtAdvanceLevel_Maths = ((double) numberOfStudentsScoredAtAdvanceLevel_Maths / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsScoredAtAdvanceLevel_Maths = 0;
+            }
+
+            numberOfStudentsTested_Reading = retrieveTotalStudentsTestedCount(race, "RACE", readingSubjectId);
+            if (numberOfStudentsTested_Reading != 0) {
+                percentageOfStudentsTested_Reading = ((double) numberOfStudentsTested_Reading / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsTested_Reading = 0;
+            }
+
+            numberOfStudentsScoredAtBasicLevel_Reading = retrieveTotalStudentsScoredAtBasicLevel(race, "RACE", readingSubjectId, "70", "79");
+            if (numberOfStudentsScoredAtBasicLevel_Reading != 0) {
+                percentageOfStudentsScoredAtBasicLevel_Reading = ((double) numberOfStudentsScoredAtBasicLevel_Reading / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsScoredAtBasicLevel_Reading = 0;
+            }
+
+            numberOfStudentsScoredAtProficentLevel_Reading = retrieveTotalStudentsScoredAtBasicLevel(race, "RACE", readingSubjectId, "80", "89");
+            if (numberOfStudentsScoredAtProficentLevel_Reading != 0) {
+                percentageOfStudentsScoredAtProficentLevel_Reading = ((double) numberOfStudentsScoredAtProficentLevel_Reading / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsScoredAtProficentLevel_Reading = 0;
+            }
+
+            numberOfStudentsScoredAtAdvanceLevel_Reading = retrieveTotalStudentsScoredAtBasicLevel(race, "RACE", readingSubjectId, "90", "99");
+            if (numberOfStudentsScoredAtAdvanceLevel_Reading != 0) {
+                percentageOfStudentsScoredAtAdvanceLevel_Reading = ((double) numberOfStudentsScoredAtAdvanceLevel_Reading / totalNumberOfStudents) * 100;
+            } else {
+                percentageOfStudentsScoredAtAdvanceLevel_Reading = 0;
+            }
+
             stateReportVO = new StateReportVO();
             stateReportVO.setSubGroupName(race);
-            stateReportVO.setTotalNumberOfStudents_Maths(retrieveTotalStudentsCount(race, "RACE"));
-            stateReportVO.setNumberOfStudentsTested_Maths(50);
-            double d = ((double) 50 / 60) * 100;
-            stateReportVO.setPercentageOfStudentsTested_Maths(d);
-            stateReportVO.setPercentageOfStudentsScoredAtAdvanceLevel_Maths(55.62);
-            stateReportVO.setPercentageOfStudentsScoredAtBasicLevel_Maths(55.62);
-            stateReportVO.setPercentageOfStudentsScoredAtProficentLevel_Maths(55.62);
-            stateReportVO.setTotalNumberOfStudents_Reading(60);
-            stateReportVO.setNumberOfStudentsTested_Reading(50);
-            stateReportVO.setPercentageOfStudentsTested_Reading((50 / 60) * 100);
-            stateReportVO.setPercentageOfStudentsScoredAtAdvanceLevel_Reading(55.62);
-            stateReportVO.setPercentageOfStudentsScoredAtBasicLevel_Reading(55.62);
-            stateReportVO.setPercentageOfStudentsScoredAtProficentLevel_Reading(55.62);
+
+            stateReportVO.setTotalNumberOfStudents_Maths(totalNumberOfStudents);
+            stateReportVO.setNumberOfStudentsTested_Maths(numberOfStudentsTested_Maths);
+            stateReportVO.setPercentageOfStudentsTested_Maths(percentageOfStudentsTested_Maths);
+            stateReportVO.setPercentageOfStudentsScoredAtBasicLevel_Maths(percentageOfStudentsScoredAtBasicLevel_Maths);
+            stateReportVO.setPercentageOfStudentsScoredAtProficentLevel_Maths(percentageOfStudentsScoredAtProficentLevel_Maths);
+            stateReportVO.setPercentageOfStudentsScoredAtAdvanceLevel_Maths(percentageOfStudentsScoredAtAdvanceLevel_Maths);
+
+            stateReportVO.setTotalNumberOfStudents_Reading(totalNumberOfStudents);
+            stateReportVO.setNumberOfStudentsTested_Reading(numberOfStudentsTested_Reading);
+            stateReportVO.setPercentageOfStudentsTested_Reading(percentageOfStudentsTested_Reading);
+            stateReportVO.setPercentageOfStudentsScoredAtBasicLevel_Reading(percentageOfStudentsScoredAtBasicLevel_Reading);
+            stateReportVO.setPercentageOfStudentsScoredAtProficentLevel_Reading(percentageOfStudentsScoredAtProficentLevel_Reading);
+            stateReportVO.setPercentageOfStudentsScoredAtAdvanceLevel_Reading(percentageOfStudentsScoredAtAdvanceLevel_Reading);
+
             stVOs.add(stateReportVO);
         }
-        this.setStateReportVOs(stVOs);
-        return null;
+        return stVOs;
     }
 
     private int retrieveTotalStudentsCount(String argSubGroupName, String argSubGroupType) {
@@ -121,26 +210,131 @@ public class StateReportController {
         return count;
     }
 
-    private int retrieveTotalStudentsTestedCount() {
+    private int retrieveTotalStudentsTestedCount(String argSubGroupName, String argSubGroupType, Integer argSubjectId) {
         int count = 0;
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String queryString = "";
+        Query query = null;
+        if ("RACE".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st where st.race=:race)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("race", argSubGroupName);
+        } else if ("ETHNICITY".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st where st.ethnicity=:ethnicity)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("ethnicity", argSubGroupName);
+        } else if ("GENDER".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st "
+                    + "where st.profile.gender=:gender)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("gender", argSubGroupName);
+        } else if ("DISABILITY".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st "
+                    + "where st.disabilitysupportneeded=:disabilitysupportneeded)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("disabilitysupportneeded", argSubGroupName);
+        }
+        List<Studentgradelevel> stglvlList = (List<Studentgradelevel>) query.getResultList();
+        if (!stglvlList.isEmpty()) {
+            count = stglvlList.size();
+        }
         return count;
     }
 
-    private int retrieveTotalStudentsScoredAtBasicLevel() {
+    private int retrieveTotalStudentsScoredAtBasicLevel(
+            String argSubGroupName,
+            String argSubGroupType,
+            Integer argSubjectId, String argStartScore, String argEndScore) {
         int count = 0;
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String queryString = "";
+        Query query = null;
+        if ("RACE".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.finalscore between :startscore and :endscore and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st where st.race=:race)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("startscore", argStartScore);
+            query.setParameter("endscore", argEndScore);
+            query.setParameter("race", argSubGroupName);
+        } else if ("ETHNICITY".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st where st.ethnicity=:ethnicity)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("ethnicity", argSubGroupName);
+        } else if ("GENDER".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st "
+                    + "where st.profile.gender=:gender)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("gender", argSubGroupName);
+        } else if ("DISABILITY".equalsIgnoreCase(argSubGroupType)) {
+            queryString = "select stscd from Studentscorecard stscd where "
+                    + "stscd.schoolyear=:schoolyear and "
+                    + "stscd.subjectid=:subjectid and "
+                    + "stscd.studentId in "
+                    + "(select st.studentid from Student st "
+                    + "where st.disabilitysupportneeded=:disabilitysupportneeded)";
+            query = entityManager.createQuery(queryString);
+            query.setParameter("schoolyear", this.selectedSchoolYear);
+            query.setParameter("subjectid", argSubjectId);
+            query.setParameter("disabilitysupportneeded", argSubGroupName);
+        }
+        List<Studentgradelevel> stglvlList = (List<Studentgradelevel>) query.getResultList();
+        if (!stglvlList.isEmpty()) {
+            count = stglvlList.size();
+        }
         return count;
     }
 
-    private int retrieveTotalStudentsScoredAtProficientLevel() {
-        int count = 0;
-        return count;
-    }
-
-    private int retrieveTotalStudentsScoredAtAdvanceLevel() {
-        int count = 0;
-        return count;
-    }
-
+//    private int retrieveTotalStudentsScoredAtProficientLevel(String argSubGroupName, String argSubGroupType, Integer argSubjectId) {
+//        int count = 0;
+//        return count;
+//    }
+//
+//    private int retrieveTotalStudentsScoredAtAdvanceLevel(String argSubGroupName, String argSubGroupType, Integer argSubjectId) {
+//        int count = 0;
+//        return count;
+//    }
     /**
      * @return the schoolyearschedules
      */
